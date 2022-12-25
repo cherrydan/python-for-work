26f6059fe95226d0b493afef08b70005bf19dcab
cherrydan <cherrydan.1978@gmail.com>
2022-12-25 21:48:15 +0300

    Catch TypeError exception

--- Iterators/HomeworkIterator.py
+++ Iterators/HomeworkIterator.py
@@ -8,8 +8,12 @@ class HomeworkIterator:
         return self
 
     def __next__(self):
-        if self.counter < len(self.some_obj):
-            result = self.some_obj[self.counter] + 10 # здесь мы берём текущее значение и прибавляем к нему 10
-            self.counter += 1  # пока не достигнут конец массива передвигаем счётчик
-            return result  # возвращаем новое значение
-        raise StopIteration
+        try:
+            if self.counter < len(self.some_obj):
+                result = self.some_obj[self.counter] + 10 # здесь мы берём текущее значение и прибавляем к нему 10
+                self.counter += 1  # пока не достигнут конец массива передвигаем счётчик
+                return result  # возвращаем новое значение
+            raise StopIteration
+        except TypeError:
+            print('Исходный массив должен состоять из чисел, не строк')
+            raise StopIteration
